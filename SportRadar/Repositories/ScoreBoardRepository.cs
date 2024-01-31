bbfc8248452a462644b9a239cd43d3b6675df529
женя гуньков <evguni0s@gmail.com>
2024-01-31 13:29:13 +0100

    Changed project type from console to library. Added data models

---
+++ SportRadar/Repositories/ScoreBoardRepository.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SportRadar.Repositories
+{
+    internal class ScoreBoardRepository
+    {
+    }
+}
