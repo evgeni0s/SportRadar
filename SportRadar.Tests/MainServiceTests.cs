6bbe8c9b965fe094f6dc96d2991d686b2c048e98
женя гуньков <evguni0s@gmail.com>
2024-01-31 16:53:34 +0100

    Start new match. Red test

--- SportRadar.Tests/MainServiceTests.cs
+++ SportRadar.Tests/MainServiceTests.cs
@@ -1,6 +1,8 @@
+using SportRadar.Models;
+
 namespace SportRadar.Tests
 {
-    public class Tests
+    public class MainServiceTests
     {
         [SetUp]
         public void Setup()
@@ -8,9 +10,19 @@ namespace SportRadar.Tests
         }
 
         [Test]
+        [TestCase()]
         public void Test1()
         {
-            Assert.Pass();
+            // Arrange
+            var mainService = new ScoreBoard();
+            var homeTeam = "Mexico";
+            var awayTeam = "Canada";
+
+            // Act
+            mainService.StartMatch(homeTeam, awayTeam);
+
+            // Assert
+            Assert.AreEqual(0, mainService.Score(homeTeam));
+            Assert.AreEqual(0, mainService.Score(awayTeam));
         }
-    }
 }
